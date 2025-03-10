dd42ae9162633236fabff481dcfd53bdc1c1970f
AntoineO-dev <antoine.oriol01@gmail.com>
2025-03-10 09:53:49 +0100

    Sixth commit, Routes GET inclure, Routes GET services, Ajout inclure et services SWAGGER

--- controllers/serviceController.js
+++ controllers/serviceController.js
@@ -1,5 +1,15 @@
 const servicesService = require('../services/serviceServices');
 
+
+async function findServiceById(req, res) {
+    try {
+        const result = await servicesService.findServiceById(req.params.id);
+        res.status(200).json(result);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+}
+
 async function findAllServices(req, res) {
     try {
         const result = await servicesService.findAllServices();
@@ -18,7 +28,28 @@ async function findServiceByPrice(req, res) {
     }
 }
 
+async function findServiceByQuantity(req, res) {
+    try {
+        const result = await servicesService.findServiceByQuantity(req.params.quantity);
+        res.status(200).json(result);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+}
+
+async function findTotalCostByType(req, res) {
+    try {
+        const result = await servicesService.findTotalCostByType(req.params.type);
+        res.status(200).json(result);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+}
+
 module.exports = {
     findAllServices,
-    findServiceByPrice
+    findServiceByPrice,
+    findServiceByQuantity,
+    findServiceById,
+    findTotalCostByType
 };
--- routes/serviceRoutes.js
+++ routes/serviceRoutes.js
@@ -8,6 +8,14 @@ router.get('/',(req,res) => {serviceController.findAllServices(req,res)});
 // GET /services/:price
 router.get('/price/:price',(req,res) => {serviceController.findServiceByPrice(req,res)});
 
-//GET /services/
+//GET /services/frequent/:quantity
+router.get('/frequent/:quantity',(req,res) => {serviceController.findServiceByQuantity(req,res)});
+
+//GET /services/total_cost/:type
+router.get('/total_cost/:type',(req,res) => {serviceController.findTotalCostByType(req,res)});
+
+
+//GET /services/:id
+router.get('/:id',(req,res) => {serviceController.findServiceById(req,res)});
 
 module.exports = router;
